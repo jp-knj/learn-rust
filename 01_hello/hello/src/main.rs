ec4b9ef9d2ea0d2a017d3895acdfe15b68cfeb1f
jp-knj <kenji.tomita1996@gmail.com>
2022-03-05 20:27:26 +0900

    :green_heart: coding test and implement

--- 01_hello/hello/src/main.rs
+++ 01_hello/hello/src/main.rs
@@ -12,4 +12,11 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
         }
         m = m % n;
     }
+    n
+}
+
+#[test]
+fn test_gcd(){
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2*3*5*11*17, 3*7*11*13*19),3*11);
 }
