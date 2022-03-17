8b730bc82a0b741f4854f889a80af0dc817ecfcc
jp-knj <kenji.tomita1996@gmail.com>
2022-03-17 20:38:26 +0900

    :+1: render a rectangle of the mandelbrot

--- 03_parallel-programming/modelbrot/src/main.rs
+++ 03_parallel-programming/modelbrot/src/main.rs
@@ -87,3 +87,23 @@ fn test_pixel_to_point() {
                               Complex { re:  1.0, im: -1.0 }),
                Complex { re: -0.5, im: -0.75 });
 }
+
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          upper_left: Complex<f64>,
+          lower_right: Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row),
+                                       upper_left, lower_right);
+            pixels[row * bounds.0 + column] =
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                };
+        }
+    }
+}
