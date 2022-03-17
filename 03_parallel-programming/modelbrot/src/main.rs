4a201004c7b9c6dfa26f8450be1ec824c565cabf
jp-knj <kenji.tomita1996@gmail.com>
2022-03-17 20:46:45 +0900

    :+1: add main fn

--- 03_parallel-programming/modelbrot/src/main.rs
+++ 03_parallel-programming/modelbrot/src/main.rs
@@ -124,3 +124,48 @@ fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
 
     Ok(())
 }
+
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT",
+                  args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                  args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    // Scope of slicing up `pixels` into horizontal bands.
+    {
+        let bands: Vec<(usize, &mut [u8])> = pixels
+            .chunks_mut(bounds.0)
+            .enumerate()
+            .collect();
+
+        bands.into_par_iter()
+            .for_each(|(i, band)| {
+                let top = i;
+                let band_bounds = (bounds.0, 1);
+                let band_upper_left = pixel_to_point(bounds, (0, top),
+                                                     upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + 1),
+                                                      upper_left, lower_right);
+                render(band, band_bounds, band_upper_left, band_lower_right);
+            });
+    }
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
+}
