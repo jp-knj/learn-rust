e1e07cc939deaa52404f531f433b7bfbe174f44e
jp-knj <kenji.tomita1996@gmail.com>
2022-03-17 20:43:18 +0900

    :+1: write the buffer pixel

--- 03_parallel-programming/modelbrot/src/main.rs
+++ 03_parallel-programming/modelbrot/src/main.rs
@@ -107,3 +107,20 @@ fn render(pixels: &mut [u8],
         }
     }
 }
+
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
+               -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels,
+                   bounds.0 as u32, bounds.1 as u32,
+                   ColorType::Gray(8))?;
+
+    Ok(())
+}
