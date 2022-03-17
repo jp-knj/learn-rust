160badaded10169956ae0a16a9d1cd40e7768a50
jp-knj <kenji.tomita1996@gmail.com>
2022-03-17 20:33:14 +0900

    :+1: parse string

--- 03_parallel-programming/modelbrot/src/main.rs
+++ 03_parallel-programming/modelbrot/src/main.rs
@@ -24,3 +24,28 @@ fn escape_time(c:Complex<f64>, limit: usize) -> Option<usize> {
     }
     None
 }
+
+use std::str::FromStr;
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",        ','), None);
+    assert_eq!(parse_pair::<i32>("10,",     ','), None);
+    assert_eq!(parse_pair::<i32>(",10",     ','), None);
+    assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
