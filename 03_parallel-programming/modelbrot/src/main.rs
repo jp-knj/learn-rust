1f76d9961e89181a6ac3577df69379624c52e5fb
jp-knj <kenji.tomita1996@gmail.com>
2022-03-17 09:02:52 +0900

    :sparkles: learn rust

--- 03_parallel-programming/modelbrot/src/main.rs
+++ 03_parallel-programming/modelbrot/src/main.rs
@@ -11,3 +11,16 @@ fn complex_square_add_loop(c: Complex<f64>){
         z = z * z + c;
     }
 }
+
+use num::Complex;
+
+fn escape_time(c:Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex{ re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_spr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
